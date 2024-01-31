1617af05eb21a5bb83ac32a4b007a26f3ceff3bf
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-01-31 15:31:48 -0600

    Added export functions to database.js

--- scripts/database.js
+++ scripts/database.js
@@ -192,5 +192,20 @@ const database = {
             genre: 'Country' 
         }
     ]
-  };
-  
+};
+
+export const getVenues = () => {
+    return database.venues.map(venue => ({...venue}))
+}
+
+export const getBands = () => {
+    return database.bands.map(band => ({...band}))
+}
+
+export const getBookings = () => {
+    return database.bookings.map(booking => ({...booking}))
+}
+
+export const getGenres = () => {
+    return database.genres.map(genre => ({...genre}))
+}
