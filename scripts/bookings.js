c77cd58bb9f25ea36fdeb7401881491b02814691
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-01 16:49:57 -0600

    Added event for clicking bookings

--- scripts/bookings.js
+++ scripts/bookings.js
@@ -1,4 +1,4 @@
-import { getBookings, getBands, getVenues } from "./database.js";
+import { getBookings, getBands, getVenues, getGenres } from "./database.js";
 
 export const bookingsList = () => {
     const bookings = getBookings()
@@ -31,7 +31,8 @@ export const bookingsList = () => {
 
         // Generate HTML for booking item
         bookingsHTML += `<li data-type="booking"
-                                date-id="${booking.id}">
+                                data-id="${booking.id}"
+                                data-bandId="${booking.bandId}">
                                 ${bandName} are playing at ${venueName} on ${bookingDate}
                         </li>`
     }
@@ -41,3 +42,30 @@ export const bookingsList = () => {
 
     return bookingsHTML
 }
+
+
+document.addEventListener("click", (clickEvent) => {
+    const eventTarget = clickEvent.target
+
+    if (eventTarget.dataset.type === "booking") {
+        const bandId = parseInt(eventTarget.dataset.bandid)
+
+        const bands = getBands()
+        let bandObj = {}
+        for (const band of bands) {
+            if (band.id === bandId) {
+                bandObj = band
+            }
+        }
+
+        const genres = getGenres()
+        let bandGenre = ""
+        for (const genre of genres) {
+            if (genre.id === bandObj.genre) {
+                bandGenre = genre.genre
+            }
+        }
+        
+        window.alert(`${bandObj.name}\n${bandGenre}\nFormed in ${bandObj.formationYear}\n${bandObj.memberCount} band member(s)`)
+    }
+})
