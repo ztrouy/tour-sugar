6978b168b1f7e16aa006ad7c4148b2edc70fd742
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-01 14:58:58 -0600

    Added HTML generating function for bookings

---
+++ scripts/bookings.js
@@ -0,0 +1,40 @@
+import { getBookings, getBands, getVenues } from "./database.js";
+
+export const bookingsList = () => {
+    const bookings = getBookings()
+    
+    // Initialize string of HTML to return
+    let bookingsHTML = "<ul>"
+
+    for (const booking of bookings) {
+        
+        // Get date of the booking
+        const bookingDate = booking.date
+
+        // Get name of the band
+        const bands = getBands()
+        let bandName = ""
+        for (const band of bands) {
+            if (booking.bandId === band.id) {
+                bandName = band.name
+            }
+        }
+
+        // Get name of the venue
+        const venues = getVenues()
+        let venueName = ""
+        for (const venue of venues) {
+            if (booking.venueId === venue.id) {
+                venueName = venue.name
+            }
+        }
+
+        // Generate HTML for booking item
+        bookingsHTML += `<li>${bandName} are playing at ${venueName} on ${bookingDate}</li>`
+    }
+
+    // Finish string of HTML to return
+    bookingsHTML += "</ul>"
+
+    return bookingsHTML
+}
