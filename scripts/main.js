0305c06948fe94cb3c0557cc297099084125f14b
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-01 16:03:19 -0600

    Added import and HTML injector for bands

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
 import { bookingsList } from "./bookings.js"
 import { venuesList } from "./venues.js"
+import { bandsList } from "./bands.js"
 
 const bookingHTML = document.querySelector("#booking-list")
 bookingHTML.innerHTML = bookingsList()
 
 const venueHTML = document.querySelector("#venue-list")
 venueHTML.innerHTML = venuesList()
+
+const bandHTML = document.querySelector("#band-list")
+bandHTML.innerHTML = bandsList()
