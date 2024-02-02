aaf4a1c0532d2f2e458292b8f4130dc8b48d64a0
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-02 11:38:29 -0600

    Adding more bookings, and modularized main HTML

--- scripts/bands.js
+++ scripts/bands.js
@@ -21,3 +21,30 @@ export const bandsList = () => {
 
     return bandsHTML
 }
+
+
+document.addEventListener("click", (clickEvent) => {
+    const eventTarget = clickEvent.target
+
+    if (eventTarget.dataset.type === "band") {
+        const bandId = parseInt(eventTarget.dataset.id)
+
+        const bookings = getBookings()
+        const venues = getVenues()
+        const venueNames = []
+
+        for (const booking of bookings) {
+            if (booking.bandId === bandId) {
+                for (const venue of venues) {
+                    if (venue.id === booking.venueId && venueNames.includes(venue.name) === false) {
+                        venueNames.push(venue.name)
+                    }
+                }
+            }
+        }
+
+        const venueNamesString = venueNames.join(", ")
+
+        window.alert(`They are playing at the following venues: ${venueNamesString}`)
+    }
+})
--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,21 @@ import { bookingsList } from "./bookings.js"
 import { venuesList } from "./venues.js"
 import { bandsList } from "./bands.js"
 
-const bookingHTML = document.querySelector("#booking-list")
-bookingHTML.innerHTML = bookingsList()
+// const bookingHTML = document.querySelector("#booking-list")
+// bookingHTML.innerHTML = bookingsList()
 
-const venueHTML = document.querySelector("#venue-list")
-venueHTML.innerHTML = venuesList()
+// const venueHTML = document.querySelector("#venue-list")
+// venueHTML.innerHTML = venuesList()
 
-const bandHTML = document.querySelector("#band-list")
-bandHTML.innerHTML = bandsList()
+// const bandHTML = document.querySelector("#band-list")
+// bandHTML.innerHTML = bandsList()
+
+const contaierHTML = document.querySelector("#container")
+contaierHTML.innerHTML = `
+<div id="upper-row">
+    <article id="booking-list" class="list">${bookingsList()}</article>
+</div>
+<div id="lower-row">
+    <article id="venue-list" class="list">${venuesList()}</article>
+    <article id="band-list" class="list">${bandsList()}</article>
+</div>`
