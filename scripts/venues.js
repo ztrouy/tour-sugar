34a84b768724705fb4fb60b847011c657247661c
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-02 00:00:39 -0600

    Added click event for venues

--- scripts/venues.js
+++ scripts/venues.js
@@ -21,3 +21,30 @@ export const venuesList = () => {
 
     return venuesHTML
 }
+
+
+document.addEventListener("click", (clickEvent) => {
+    const eventTarget = clickEvent.target
+
+    if (eventTarget.dataset.type === "venue") {
+        const venueId = parseInt(eventTarget.dataset.id)
+
+        const bookings = getBookings()
+        const bands = getBands()
+        const bandNames = []
+
+        for (const booking of bookings) {
+            if (booking.venueId === venueId) {
+                for (const band of bands) {
+                    if (booking.bandId === band.id && bandNames.includes(band.name) === false) {
+                        bandNames.push(band.name)
+                    }
+                }
+            }
+        }
+
+        const bandNamesString = bandNames.join(", ")
+
+        window.alert(`The following bands will be playing here: ${bandNamesString}`)
+    }
+})
